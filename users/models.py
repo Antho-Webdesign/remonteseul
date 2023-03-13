b2edbf902d4727d6dda3907dc52b1b61241d5f6a
AnthoWebDesign <toutchy.kipik@outlook.com>
2023-03-14 00:37:41 +0100

    models appareil ok

--- users/models.py
+++ users/models.py
@@ -2,11 +2,13 @@ from django.db import models
 from django.contrib.auth.models import User
 from PIL import Image
 
+from boutique.models import Appareils
+
 
 # Extending User Model Using a One-To-One Link
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-
+    list_devices = models.ManyToManyField(Appareils, null=True)
     avatar = models.ImageField(default='default.jpg', upload_to='profile_images')
     bio = models.TextField()
 
